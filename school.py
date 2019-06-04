35b7ac2c5550bc832edcbb9dc19460c68884c1f3
Dennis Trimarchi <dtuk81@gmail.com>
2019-06-04 01:15:22 -0400

    completed lab

--- school.py
+++ school.py
@@ -1,1 +1,23 @@
 class School:
+  def __init__(self, name=None, roster={}):
+    self.name=name
+    self.roster=roster
+
+  def add_student(self, name, grade):
+    self.roster.setdefault(grade,[]).append(name)
+
+  def grade(self, grade):
+    return self.roster[grade]
+
+  def sort_roster(self):
+    for key in self.roster.keys():
+      self.roster[key].sort()
+
+    return self.roster
+
+
+
+
+
+
+
